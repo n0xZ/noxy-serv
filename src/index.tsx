a4c014fefa4d141712b62077f8c96e73410f2e1f
Gon <o.gonzalo@hotmail.com>
2025-09-06 00:55:43 -0300

    feat: Add query and router provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { Router } from '@solidjs/router'
 import App from "./App";
 import "./tailwind.css";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+import { QueryClientProvider, QueryClient } from "@tanstack/solid-query";
+const queryClient = new QueryClient();
+render(() =>
+    <QueryClientProvider client={queryClient}>
+        <Router>
+            <App />
+        </Router>
+    </QueryClientProvider>
+    , document.getElementById("root") as HTMLElement);
