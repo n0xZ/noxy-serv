f3ca09cdcb9ce8a78b48cd8e234d6e1f26fc1673
Gon <o.gonzalo@hotmail.com>
2025-09-07 10:58:07 -0300

    feat: add global layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { Route } from '@solidjs/router'
-import { lazy } from 'solid-js';
+import { Route } from "@solidjs/router";
+import { lazy } from "solid-js";
 
-const Main = lazy(() => import("./views/main"))
-const ServiceOverviewPage = lazy(() => import("./views/services/[id]/overview"))
-const ServicesOverviewPage = lazy(() => import("./views/services/overview"))
+const Main = lazy(() => import("./views/main"));
+const ServiceOverviewPage = lazy(
+	() => import("./views/services/[id]/overview"),
+);
+const ServicesOverviewPage = lazy(() => import("./views/services/overview"));
 const CreateServicePage = lazy(() => import("./views/services/create"));
 
 function App() {
-
-  return (
-
-    <>
-      <Route path="/" component={Main} />
-      <Route path="/services/overview" component={ServicesOverviewPage} />
-      <Route path="/services/:id/overview" component={ServiceOverviewPage} />
-      <Route path="/services/create" component={CreateServicePage} />
-    </>
-  );
+	return (
+		<>
+			<Route path="/" component={Main} />
+			<Route path="/services/overview" component={ServicesOverviewPage} />
+			<Route path="/services/:id/overview" component={ServiceOverviewPage} />
+			<Route path="/services/create" component={CreateServicePage} />
+		</>
+	);
 }
 
 export default App;
---
+++ src/views/main.tsx
@@ -0,0 +1,6 @@
+import { JSX } from "solid-js";
+
+const MainPage = (): JSX.Element => {
+	return <section>main!</section>;
+};
+export default MainPage;
