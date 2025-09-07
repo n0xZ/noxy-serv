bbbf0c6d9be0cfd8c97c1a9f8f0eff6ea04d3627
Gon <o.gonzalo@hotmail.com>
2025-09-07 19:37:16 -0300

    fix: set root layout on router comp

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { Router } from '@solidjs/router'
+import { Router } from "@solidjs/router";
 import App from "./App";
 import "./tailwind.css";
-
 import { QueryClientProvider, QueryClient } from "@tanstack/solid-query";
+import AppLayout from "./ui/layout/layout";
+
 const queryClient = new QueryClient();
-render(() =>
-    <QueryClientProvider client={queryClient}>
-        <Router>
-            <App />
-        </Router>
-    </QueryClientProvider>
-    , document.getElementById("root") as HTMLElement);
+
+render(
+	() => (
+		<QueryClientProvider client={queryClient}>
+			<Router root={AppLayout}>
+				<App />
+			</Router>
+		</QueryClientProvider>
+	),
+	document.getElementById("root") as HTMLElement,
+);
